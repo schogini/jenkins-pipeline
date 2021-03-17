ab3cd4f725a61c71817df475067117d5bbdb268f
Sreeprakash Neelakantan <sree@schogini.com>
2021-03-18 00:00:22 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ if [ $qq -gt 0 ]; then
 	docker service update --env-add WEB=$2 --env-add CUSTOM_STRING=$1 --env-add BUILD_TAG=$2 --image $1 tmp1-svc2
 else
 	echo "Deploying Application.. $1"
-	echo "docker service create --name tmp1-svc2 --replicas 2 --publish 8082:8080 --env WEB=$2 $1 --env CUSTOM_STRING=$1 --env BUILD_TAG=$2"
-	docker service create --name tmp1-svc2 --replicas 2 --publish 8082:8080 --env WEB=$2 $1 --env CUSTOM_STRING=$1 --env BUILD_TAG=$2
+	echo "docker service create --name tmp1-svc2 --replicas 2 --publish 8082:8080 --env WEB=$2 --env CUSTOM_STRING=$1 --env BUILD_TAG=$2 $1"
+	docker service create --name tmp1-svc2 --replicas 2 --publish 8082:8080 --env WEB=$2 --env CUSTOM_STRING=$1 --env BUILD_TAG=$2 $1 
 fi
